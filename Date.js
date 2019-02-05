2c647f5029a2d5685e5eb878baf1f4ea61c05033
Lloyd Hardy <lloyd@lloydhardy.com>
2019-02-05 13:53:06 +0700

    Update Date.js

--- Date.js
+++ Date.js
@@ -1,1 +1,1 @@
-console.log(Date()); // Outputs date now, eg. "Tue Feb 05 2019 13:49:08 GMT+0700 (Indochina Time)"
+console.log(Date()); // Outputs date now to console, eg. "Tue Feb 05 2019 13:49:08 GMT+0700 (Indochina Time)"
