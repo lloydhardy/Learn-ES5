1ce7bb0eb1c142dda9224b13df98c5e4ce0d1703
Lloyd Hardy <lloyd@lloydhardy.com>
2019-02-06 11:07:10 +0700

    Update console.log.js

--- console.log.js
+++ console.log.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>My Webpage</title>
+</head>
+<body>
+  <script>
+    console.log("Hello World!"); // Outputs 'Helo World!' to the Javascript Console
+  </script>
+</body>
+</html>
