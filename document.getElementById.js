514c3c4d259d67adf4b71ecbe470db5ce5f90c57
Lloyd Hardy <lloyd@lloydhardy.com>
2019-02-06 11:31:00 +0700

    Update and rename document.getElementById.html to document.getElementById.js

---
+++ document.getElementById.js
@@ -0,0 +1,4 @@
+  document.write('<div id="myDiv">');
+  document.write('Hello World!');
+  document.write('</div>');
+  alert(document.getElementById('myDiv').innerHTML); // Displays an alert box with 'Hello World!'
