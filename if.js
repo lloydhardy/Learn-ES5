14ec3520118fa532c291925955ae75e54672688c
Lloyd Hardy <lloyd@lloydhardy.com>
2019-02-06 11:29:32 +0700

    Update and rename if.html to if.js

---
+++ if.js
@@ -0,0 +1,8 @@
+  if (1 == 1)
+    {
+      console.log('True'); // Outputs 'True' to console if the statement (1 == 1) is true, which it is
+    }
+  else
+    {
+      console.log('False'); // This does not execute, however if the staement were false, eg. (1 == 2), 'False' would be output to console
+    }
